c5ebe27f7177217d2193227b536526c6f4c9f15d
Jefin Jacob <64959024+Jefin-J@users.noreply.github.com>
2022-11-22 13:39:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import "./App.css";
-import { BsSearch } from "react-icons/bs";
-import WeatherBox from "./weatherBox";
 
 function App() {
-  const search = () => {
-    console.log("Search");
-  };
+
   return (
     <div className="App">
-      <main>
         <div className="search-box">
           <input
-            className="search-bar"
-            type="text"
-            placeholder="Search..."
-            onKeyDown={(evt) => (evt.key === "Enter" ? search() : "")}
-          />
-          <button className="search-button" onClick={search}>
-            <BsSearch />
-          </button>
+            className="search-bar" type="text" placeholder="Search..."/>
         </div>
-
-        <WeatherBox place="Ernakulam" />
-      </main>
     </div>
   );
 }
